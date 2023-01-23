345a0ae416c5b65e9900d663842cec9a4797f378
saina <saina.pourjafari@gmail.com>
2023-01-23 18:36:38 +0330

    doxygen is left

--- projectm.c
+++ projectm.c
@@ -1,15 +1,35 @@
-///khat haye khata(baraye jump)
+/// khat haye khata(baraye jump)
 
 #include <stdio.h>
 #include <string.h>
-#include <limits.h>
+// #include <limits.h>
 #include <stdlib.h>
 #include <ctype.h>
+// #include<windows.h>  //colors
+
+// colors for errors
+void red()
+{
+    printf("\033[0;31m");
+}
+
+void yellow()
+{
+    printf("\033[0;33m");
+}
+
+void whiteB()
+{
+    printf("\033[1;37m");
+}
+
+void reset()
+{
+    printf("\033[0m");
+}
 
 int array[32] = {0};
-int sabbat[8]={2,2,2,2,2,2,2,2};
-
-//sabbat[3]=3;?????????
+int sabbat[8] = {2, 2, 2, 2, 2, 2, 2, 2};
 
 //(start)sabbat vaziat ha
 void ParityFlag(int result)
@@ -176,7 +196,7 @@ void ORI(int ori, int a, int imm)
 void MOV(int rt, int imm)
 {
     array[rt] = imm;
-    //printf("%d", array[rt]);
+    // printf("%d", array[rt]);
 }
 
 void SWP(int a1, int a2)
@@ -188,14 +208,17 @@ void SWP(int a1, int a2)
 
 void DUMP_REGS()
 {
-    printf("\n array:");
+    whiteB();
+    printf("\n ARRAY:");
+    reset();
 
     for (int i = 0; i < 32; i++)
     {
         printf("%d  ", array[i]);
     }
-
-    printf("\n sabbat vaziat ha(remember:the value 2 does not matter): ");
+    whiteB();
+    printf("\n SABBAT VAZIAT HA(remember:the value 2 does not matter): ");
+    reset();
 
     for (int i = 0; i < 8; i++)
     {
@@ -207,26 +230,18 @@ void DUMP_REGS_F()
 {
     FILE *file;
     file = fopen("regs.txt", "w");
-    
-    // if(file==NULL)
-    // {
-    //     printf("error");  //error dar asl nist
-    // }
-    // else
-    // {
-        fprintf(file,"array:");
-        for(int i=0;i<32;i++)
-        {
-            fprintf(file,"%d ",array[i]);
-        }
 
-        fprintf(file,"\nsabbat vaziat ha:");
-        for(int i=0;i<8;i++)
-        {
-            fprintf(file,"%d ",sabbat[i]);
-        }
-    // }
+    fprintf(file, "array:");
+    for (int i = 0; i < 32; i++)
+    {
+        fprintf(file, "%d ", array[i]);
+    }
 
+    fprintf(file, "\nsabbat vaziat ha:");
+    for (int i = 0; i < 8; i++)
+    {
+        fprintf(file, "%d ", sabbat[i]);
+    }
 }
 
 void INPUT()
@@ -238,14 +253,6 @@ void OUTPUT()
 {
     printf("%d", array[0]);
 }
-
-// void JMPImm()
-// {
-// }
-
-// void EXIT()
-// {
-// }
 
 void DIV(int a1, int a2)
 {
@@ -269,7 +276,7 @@ void MULL(int a1, int a2)
     array[a1] = product & temp2;
     array[a2] = product & temp1;
 
-    array[a1]=array[a1]>>4;
+    array[a1] = array[a1] >> 4;
 
     ParityFlag(product);
     ZeroFlag(product);
@@ -280,61 +287,88 @@ void MULL(int a1, int a2)
 int stack[50];
 void PUSH(int n)
 {
-    for(int i=0;i<50;i++)
+    for (int i = 0; i < 50; i++)
     {
-        stack[i+1]=stack[i];
+        stack[i + 1] = stack[i];
     }
-    stack[0]=sabbat[n];
+    stack[0] = sabbat[n];
 }
 
 void POP(int n)
 {
-    sabbat[n]=stack[0];
-    for(int i=0;i<50;i++)
+    sabbat[n] = stack[0];
+    for (int i = 0; i < 50; i++)
     {
-        stack[i]=stack[i+1];
+        stack[i] = stack[i + 1];
     }
 }
 //(end)functions
 
-int main(int argc,char* argv[])
+//(start)Errors
+int errorAndis(int a, int b, int c, char dastoor[])
 {
-    printf("the file name is:\n %s",argv[1]);
+    int checkAndis = 1;
+    if (a < 0 || b < 0 || c < 0)
+    {
+        red();
+        printf("Error...you have negative arguement in %s", dastoor);
+        reset();
+        checkAndis = 0;
+    }
+    if (a > 31 || b > 31 || c > 31)
+    {
+        red();
+        printf("Error...you have arguement more than 31 in %s(your array can only have 32 elements)", dastoor);
+        reset();
+        checkAndis = 0;
+    }
 
-    // char name[100];
-    int countW=0; //
+    return checkAndis;
+}
+
+int main(int argc, char *argv[])
+{
+    int checkAllLine = 0;
+    int countJmp = 0;
+
+    int countW = 0; //
     char buffer[1000];
 
-
-    // printf("Enter the file name:");
-    // scanf("%s",name);
     FILE *voroodi;
-    if(argc<2)
+    if (argc < 2)
     {
-        voroodi=fopen("in.txt","r");
+        printf("the file name is: in.txt\n");
+        voroodi = fopen("in.txt", "r");
     }
     else
     {
-        voroodi=fopen(argv[1],"r");
+        printf("the file name is: %s\n", argv[1]);
+        voroodi = fopen(argv[1], "r");
     }
-    // voroodi = fopen(name, "r");
 
     while (fscanf(voroodi, "%[^\n]\ns", buffer) != EOF)
     {
-        int a1,a2,a3;
-        countW++;  //
+        checkAllLine++;
+    }
+
+    rewind(voroodi);
+
+    while (fscanf(voroodi, "%[^\n]\ns", buffer) != EOF)
+    {
+        int a1, a2, a3;
+        countW++; //
         for (int i = 0; i < sizeof(buffer); i++)
         {
             buffer[i] = toupper(buffer[i]);
         }
 
-        char dastoor[15]={'\0'};
+        char dastoor[15] = {'\0'};
         int j;
         for (j = 0; buffer[j] != ' '; j++)
         {
             dastoor[j] = buffer[j];
         }
-       // dastoor[j+1] ='\0' ;
+        // dastoor[j+1] ='\0' ;
 
         if (strcmp(dastoor, "EXIT") == 0)
         {
@@ -343,94 +377,107 @@ int main(int argc,char* argv[])
 
         else if (strcmp(dastoor, "ADD") == 0)
         {
-            
             sscanf(buffer, "ADD S%d, S%d, S%d", &a3, &a1, &a2);
-            ADD(a3, a1, a2);
+            if (errorAndis(a3, a1, a2, dastoor) == 1)
+                ADD(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "SUB") == 0)
         {
-        
+
             sscanf(buffer, "SUB S%d, S%d, S%d", &a3, &a1, &a2);
-            SUB(a3, a1, a2);
+            if (errorAndis(a3, a1, a2, dastoor) == 1)
+                SUB(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "AND") == 0)
         {
-           
+
             sscanf(buffer, "AND S%d, S%d, S%d", &a3, &a1, &a2);
-            AND(a3, a1, a2);
+            if (errorAndis(a3, a1, a2, dastoor) == 1)
+                AND(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "XOR") == 0)
         {
-            
+
             sscanf(buffer, "XOR S%d, S%d, S%d", &a3, &a1, &a2);
-            XOR(a3, a1, a2);
+            if (errorAndis(a3, a1, a2, dastoor) == 1)
+                XOR(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "OR") == 0)
         {
-            
-            sscanf(buffer, "OR S%d, S%d, S%d", & a3, &a1, &a2);
-            OR(a3, a1, a2);
+
+            sscanf(buffer, "OR S%d, S%d, S%d", &a3, &a1, &a2);
+            if (errorAndis(a3, a1, a2, dastoor) == 1)
+                OR(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "ADDI") == 0)
         {
-           
+
             sscanf(buffer, "ADDI S%d, S%d, %d", &a3, &a1, &a2);
-            ADDI(a3, a1, a2);
+            if (errorAndis(a3, a1, 0, dastoor) == 1)
+                ADDI(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "SUBI") == 0)
         {
-           
+
             sscanf(buffer, "SUBI S%d, S%d, %d", &a3, &a1, &a2);
-            SUBI(a3, a1, a2);
+            if (errorAndis(a3, a1, 0, dastoor) == 1)
+                SUBI(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "ANDI") == 0)
         {
-           
+
             sscanf(buffer, "ANDI S%d, S%d, %d", &a3, &a1, &a2);
-            ANDI(a3, a1, a2);
+            if (errorAndis(a3, a1, 0, dastoor) == 1)
+                ANDI(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "XORI") == 0)
         {
-            
+
             sscanf(buffer, "XORI S%d, S%d, %d", &a3, &a1, &a2);
-            XORI(a3, a1, a2);
+            if (errorAndis(a3, a1, 0, dastoor) == 1)
+                XORI(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "ORI") == 0)
         {
-           
+
             sscanf(buffer, "ORI S%d, S%d, %d", &a3, &a1, &a2);
-            ORI(a3, a1, a2);
+            if (errorAndis(a3, a1, 0, dastoor) == 1)
+                ORI(a3, a1, a2);
         }
 
         else if (strcmp(dastoor, "MOV") == 0)
         {
-            
+
             if (buffer[8] == 'S' || buffer[9] == 'S')
             {
                 sscanf(buffer, "MOV S%d, S%d", &a1, &a2);
-                MOV(a1, array[a2]);
+                if (errorAndis(0, a1, a2, dastoor) == 1)
+                    MOV(a1, array[a2]);
             }
             else
             {
+
                 sscanf(buffer, "MOV S%d, %d", &a1, &a2);
-                MOV(a1, a2);
+                if (errorAndis(0, a1, 0, dastoor) == 1)
+                    MOV(a1, a2);
             }
         }
 
         else if (strcmp(dastoor, "SWP") == 0)
         {
-           
+
             sscanf(buffer, "SWP S%d, S%d", &a1, &a2);
-            SWP(a1, a2);
+            if (errorAndis(0, a1, a2, dastoor) == 1)
+                SWP(a1, a2);
         }
 
         else if (strcmp(dastoor, "DUMP_REGS") == 0)
@@ -453,88 +500,124 @@ int main(int argc,char* argv[])
             OUTPUT();
         }
 
-        else if (strcmp(dastoor, "JMP") == 0) 
+        else if (strcmp(dastoor, "JMP") == 0)
         {
-          int countL=1,countCH=0;
-           sscanf(buffer,"JMP %d",&a1);
-           rewind(voroodi);
-           while(countL != a1)
-           {
-            countCH++;
-            if(fgetc(voroodi)=='\n')
-                countL++;
-           }
-           fseek(voroodi,countCH+1,SEEK_SET);
-           fscanf(voroodi, "%[^\n]\n",buffer);
+            countJmp++;
+            if (countJmp > 5)
+            {
+                yellow();
+                printf("you had a loop because of jump command so we ended this chookh after a few times!");
+                reset();
+                fscanf(voroodi, "%[^\n]\n", buffer);
+            }
+            else
+            {
+                int countL = 1, countCH = 0;
 
-           for(j=0;buffer[j] !=' ';j++)
-           {
-            dastoor[j]=buffer[j];
-           }
-           dastoor[j]='\0';
+                sscanf(buffer, "JMP %d", &a1);
+               
+                if(a1<=0)
+                {
+                    red();
+                    printf("Error...your lines start from 1 !");
+                    reset();
+                }
 
-            // if(imm>countW)
-            // {
+                else if(a1>checkAllLine)
+                {
+                    red();
+                    printf("Error...you only have %d lines!",checkAllLine);
+                    reset();
+                }
 
-            // }
+                else
+                {
+                    rewind(voroodi);
+                    while (countL != a1)
+                    {
+                        countCH++;
+                        if (fgetc(voroodi) == '\n')
+                            countL++;
+                    }
+                    fseek(voroodi, countCH + 1, SEEK_SET);
+                    fscanf(voroodi, "%[^\n]\n", buffer);
 
+                    for (j = 0; buffer[j] != ' '; j++)
+                    {
+                        dastoor[j] = buffer[j];
+                    }
+                    dastoor[j] = '\0';
+
+                    // if(imm>countW)
+                    // {
+
+                    // }
+                }
+                
+            }
         }
 
-        else if(strcmp(dastoor,"SKIE")==0)
+        else if (strcmp(dastoor, "SKIE") == 0)
         {
 
-            sscanf(buffer,"SKIE S%d, S%d",&a1,&a2);
-            if(array[a1]==array[a2])
+            sscanf(buffer, "SKIE S%d, S%d", &a1, &a2);
+            if (errorAndis(0, a1, a2, dastoor) == 1)
             {
-                countW++; //
-                fscanf(voroodi ,"%[^\n]\n",buffer);
+                if (array[a1] == array[a2])
+                {
+                    countW++; //
+                    fscanf(voroodi, "%[^\n]\n", buffer);
 
-            //     for(j=0;buffer[j] !=' ';j++)
-            //     {
-            //         dastoor[j]=buffer[j];
-            //     }
-            //    // dastoor[j]='\0';
+                    //     for(j=0;buffer[j] !=' ';j++)
+                    //     {
+                    //         dastoor[j]=buffer[j];
+                    //     }
+                    //    // dastoor[j]='\0';
+                }
             }
-
-         
         }
 
         else if (strcmp(dastoor, "MULL") == 0)
         {
-           
+
             sscanf(buffer, "MULL S%d, S%d", &a1, &a2);
-            MULL(a1, a2);
+            if (errorAndis(0, a1, a2, dastoor) == 1)
+                MULL(a1, a2);
         }
 
         else if (strcmp(dastoor, "DIV") == 0)
         {
-            
+
             sscanf(buffer, "DIV S%d, S%d", &a1, &a2);
-            DIV(a1, a2);
+            if (errorAndis(0, a1, a2, dastoor) == 1)
+                DIV(a1, a2);
         }
 
         else if (strcmp(dastoor, "POP") == 0)
         {
-            
+
             sscanf(buffer, "POP S%d", &a1);
-            POP(a1);
+            if (errorAndis(0, a1, 0, dastoor) == 1)
+                POP(a1);
         }
 
         else if (strcmp(dastoor, "PUSH") == 0)
         {
-            
+
             sscanf(buffer, "PUSH S%d", &a1);
-            PUSH(a1);
+            if (errorAndis(0, a1, 0, dastoor) == 1)
+                PUSH(a1);
         }
 
         else
         {
-            printf("line %d:wrong input\n",countW);
+            red();
+            printf("line %d:wrong input,error in command.\n", countW);
+            reset();
         }
     }
 
     fclose(voroodi);
-
 
     return (0);
 }
